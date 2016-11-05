09e089b348b51a19cbc811f1b6b6271cfcd97fb1
BelieveC <cayadav2009@gmail.com>
2016-11-05 23:09:25 +0530

    Add timeslot controller

---
+++ app/controllers/timeslots_controller.rb
@@ -0,0 +1,36 @@
+class TimeslotsController < ApplicationController
+	before_action :get_timeslot, only:[:edit,:create]
+
+	def new
+		@timeslot = Timeslot.new
+	end
+
+	def edit
+	end
+
+	def create
+		@timeslot = Timeslot.new(timeslot_params)
+		if @timeslot.save
+			redirect_to @timeslot
+		else
+			render 'new'
+		end
+	end
+
+	def update 
+		if @timeslot.update(timeslot_params)
+			redirect_to @timeslot
+		else 
+			render 'edit'
+		end
+	end
+
+	private
+		def timeslot_params
+			params.require(:timeslot).permite(:starttime,:endtime)
+		end
+
+		def get_timeslot
+			@timeslot = Timeslot.find(params[:id])
+		end
+end
