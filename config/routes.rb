7a6bfc4277b8b637192cd49dbe2a2588bfe7e6d9
BelieveC <cayadav2009@gmail.com>
2016-11-06 18:58:19 +0530

    Add routes for checking timeslot

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :dock_workers
   resources :timeslots
 
+  get '/check',:to=>'appointments#check',:as=> "check_availability"
   get '/dock_workers/:id/archived', :to => 'dock_workers#archived', :as => :archived
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
