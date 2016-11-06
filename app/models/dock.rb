245c0827abedbae13ceab5166f228679cf0ae692
BelieveC <cayadav2009@gmail.com>
2016-11-06 10:20:04 +0530

    fix association bug of timeslot and dock

--- app/models/dock.rb
+++ app/models/dock.rb
@@ -1,5 +1,5 @@
 class Dock < ApplicationRecord
-  has_many :reservation
+  has_many :reservations
   has_many :appointments, :through => :reservations
-  has_and_belongs_to_many :timeslot
+  has_and_belongs_to_many :timeslots
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,4 @@
 class Timeslot < ApplicationRecord
 	has_and_belongs_to_many :dock_workers
-	has_and_belongs_to_many :dock_id
+	has_and_belongs_to_many :docks
 end
