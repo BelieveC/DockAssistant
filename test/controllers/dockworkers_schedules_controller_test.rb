e4595c96e553a7038075258770b4561d57640d05
BelieveC <cayadav2009@gmail.com>
2016-11-06 22:06:16 +0530

    Add worker_schedules_controller;'

---
+++ test/controllers/dockworkers_schedules_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DockworkersSchedulesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/worker_schedules_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WorkerSchedulesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
