14463dc5924f5d76c14a8303583659250c9d93c8
BelieveC <cayadav2009@gmail.com>
2016-11-06 23:56:36 +0530

    Add worker_schedule_controller with create and destroy action

--- app/controllers/worker_schedules_controller.rb
+++ app/controllers/worker_schedules_controller.rb
@@ -1,2 +1,31 @@
 class WorkerSchedulesController < ApplicationController
+
+	def create
+		@worker = DockWorker.find(params[:worker_id])
+		@appointment = Appointment.find(params[:appointment_id])
+
+		@worker.worker_schedules.create(timeslot_id: params[:timeslot_id])
+		@appointment.shipments.create(dock_worker_id: @worker.id)
+		respond_to do |format|
+			format.js
+		end
+	end
+
+	def destroy
+		@worker = DockWorker.find(params[:worker_id])
+		@timeslot = @worker.worker_schedules.where(timeslot_id: params[:timeslot_id]).first
+		@timeslot.destroy
+		puts params[:worker_id]
+		puts params[:timeslot_id]
+		puts params[:appointment_id]
+		@appointment = Appointment.find(params[:appointment_id])
+		@shipment = @appointment.shipments.where(dock_worker_id: params[:worker_id]).first
+		@shipment.destroy
+		respond_to do |format|
+			format.js
+		end
+	end
+
+	def demo
+	end
 end
