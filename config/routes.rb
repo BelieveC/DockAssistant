e9ea25d0ed4f3824d25f643cd3f023d0692aa67b
BelieveC <cayadav2009@gmail.com>
2016-11-06 23:57:55 +0530

    Add routes for adding and removing worker for an appointment

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get '/check',:to=>'appointments#check',:as=> "check_availability"
   get '/dock_workers/:id/archived', :to => 'dock_workers#archived', :as => :archived
 
+  post '/add_worker'=>'worker_schedules#create',:as=>"add_worker"
+  match '/remove_worker'=>'worker_schedules#destroy',:as=>"remove_worker",via:[:post,:delete]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dock_workers#index'
 
