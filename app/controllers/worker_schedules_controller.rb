84fd722b1426b2266e95dbec2b45de493abd4b81
BelieveC <cayadav2009@gmail.com>
2016-11-07 00:40:37 +0530

    Fix worker_schedules_controller

--- app/controllers/worker_schedules_controller.rb
+++ app/controllers/worker_schedules_controller.rb
@@ -3,7 +3,7 @@ class WorkerSchedulesController < ApplicationController
 	def create
 		@worker = DockWorker.find(params[:worker_id])
 		@appointment = Appointment.find(params[:appointment_id])
-
+		@timeslot = Timeslot.find(params[:timeslot_id])
 		@worker.worker_schedules.create(timeslot_id: params[:timeslot_id])
 		@appointment.shipments.create(dock_worker_id: @worker.id)
 		respond_to do |format|
@@ -13,8 +13,9 @@ class WorkerSchedulesController < ApplicationController
 
 	def destroy
 		@worker = DockWorker.find(params[:worker_id])
-		@timeslot = @worker.worker_schedules.where(timeslot_id: params[:timeslot_id]).first
-		@timeslot.destroy
+		@timeslot = Timeslot.find(params[:timeslot_id])
+		@worker_schedule = @worker.worker_schedules.where(timeslot_id: params[:timeslot_id]).first
+		@worker_schedule.destroy
 		puts params[:worker_id]
 		puts params[:timeslot_id]
 		puts params[:appointment_id]
