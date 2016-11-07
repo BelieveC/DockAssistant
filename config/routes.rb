165357c295d95a75ad8cab74ca6faf1d7ac268ce
BelieveC <cayadav2009@gmail.com>
2016-11-07 13:50:17 +0530

    Remove shipments and reservation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :reservations
+ 
   resources :appointments
   resources :vendors
   resources :docks
-  resources :shipments
   resources :states
   resources :dock_workers
-  resources :timeslots
 
   get '/check',:to=>'appointments#check',:as=> "check_availability"
   get '/dock_workers/:id/archived', :to => 'dock_workers#archived', :as => :archived
