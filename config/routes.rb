e5d5a6bd9d3648268a34e294791e6f6233a4693a
BelieveC <cayadav2009@gmail.com>
2016-11-12 01:43:12 +0530

    Add routes for make_payment in dock controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get ':id/dock_report/',:to=>'docks#report',:as=>'dock_report'
   get ':id/dock_worker_report/',:to=>'dock_workers#report',:as=>'dock_worker_report'
   get ':id/dock_worker_payment/',:to=>'dock_workers#payment',:as=>'dock_worker_payment'
+  get ':id/make_payment/',:to=>'dock_workers#make_payment',:as=>'make_payment'
 
   post '/add_worker'=>'worker_schedules#create',:as=>"add_worker"
   match '/remove_worker'=>'worker_schedules#destroy',:as=>"remove_worker",via:[:post,:delete]
