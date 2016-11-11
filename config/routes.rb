4cd5ee38245fb608f2d728377fbc8bb417f6fe9c
BelieveC <cayadav2009@gmail.com>
2016-11-11 23:53:19 +0530

    Add routes for dock_worker_payment

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/dock_workers/:id/archived', :to => 'dock_workers#archived', :as => :archived
   get ':id/dock_report/',:to=>'docks#report',:as=>'dock_report'
   get ':id/dock_worker_report/',:to=>'dock_workers#report',:as=>'dock_worker_report'
+  get ':id/dock_worker_payment/',:to=>'dock_workers#payment',:as=>'dock_worker_payment'
 
   post '/add_worker'=>'worker_schedules#create',:as=>"add_worker"
   match '/remove_worker'=>'worker_schedules#destroy',:as=>"remove_worker",via:[:post,:delete]
