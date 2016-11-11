4f2f67b2fbb48c7c95d160bf0f8fd91afcf98de3
BelieveC <cayadav2009@gmail.com>
2016-11-11 21:48:05 +0530

    Add routes for dock_worker and dock report

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/check',:to=>'appointments#check',:as=> "check_availability"
   get '/dock_workers/:id/archived', :to => 'dock_workers#archived', :as => :archived
+  get ':id/dock_report/',:to=>'docks#report',:as=>'dock_report'
+  get ':id/dock_worker_report/',:to=>'dock_workers#report',:as=>'dock_worker_report'
 
   post '/add_worker'=>'worker_schedules#create',:as=>"add_worker"
   match '/remove_worker'=>'worker_schedules#destroy',:as=>"remove_worker",via:[:post,:delete]
