ef4cf5ba416ebf2b8cee0c505e2b04471b93fe80
BelieveC <cayadav2009@gmail.com>
2016-11-12 12:36:05 +0530

    Fix routes for dock and dock_workers report on index page

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   get '/check',:to=>'appointments#check',:as=> "check_availability"
   get '/dock_workers/:id/archived', :to => 'dock_workers#archived', :as => :archived
-  get ':id/dock_report/',:to=>'docks#report',:as=>'dock_report'
-  get ':id/dock_worker_report/',:to=>'dock_workers#report',:as=>'dock_worker_report'
+  get 'dock_report/',:to=>'docks#report',:as=>'dock_report'
+  get 'dock_worker_report/',:to=>'dock_workers#report',:as=>'dock_worker_report'
   get ':id/dock_worker_payment/',:to=>'dock_workers#payment',:as=>'dock_worker_payment'
   get ':id/make_payment/',:to=>'dock_workers#make_payment',:as=>'make_payment'
 
