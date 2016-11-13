96e1b083ecb2bc827290db5d68f28d6f04fbd76c
BelieveC <cayadav2009@gmail.com>
2016-11-13 11:09:33 +0530

    Add route for avg_payroll ajax

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'dock_worker_report/',:to=>'dock_workers#report',:as=>'dock_worker_report'
   get ':id/dock_worker_payment/',:to=>'dock_workers#payment',:as=>'dock_worker_payment'
   get ':id/make_payment/',:to=>'dock_workers#make_payment',:as=>'make_payment'
+  get '/avg_payroll',:to=>'dock_workers#avg_payroll',:as=>'avg_payroll'
 
   post '/add_worker'=>'worker_schedules#create',:as=>"add_worker"
   match '/remove_worker'=>'worker_schedules#destroy',:as=>"remove_worker",via:[:post,:delete]
