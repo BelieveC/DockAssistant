c5998d7905b97f102a7783d55c37c78352f2604a
BelieveC <cayadav2009@gmail.com>
2016-11-13 14:21:24 +0530

    Add validation for dock model

--- app/models/dock.rb
+++ app/models/dock.rb
@@ -4,4 +4,8 @@ class Dock < ApplicationRecord
   # has_many :timeslots, :through => :dock_schedules
   has_many :timeslots, :through => :appointments
   has_many :appointments
+
+  validates :name,presence: true,
+					length: {minimum: 5},
+					uniqueness: true
 end
