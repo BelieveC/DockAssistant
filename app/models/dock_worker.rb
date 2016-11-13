c89cda5bd5a63f7745cf7a3a2f76004408c7af04
BelieveC <cayadav2009@gmail.com>
2016-11-13 14:22:01 +0530

    Add validation for dock_worker model

--- app/models/dock_worker.rb
+++ app/models/dock_worker.rb
@@ -6,6 +6,15 @@ class DockWorker < ApplicationRecord
   has_many :shipments
   has_many :appointments, :through => :shipments
 
-  validates_uniqueness_of :last_name, scope: :first_name,
-                          message: 'A worker with that first and last name and address is already in the system.'
+  validates :first_name,uniqueness: {scope: :last_name,message:"Last and First Name already taken"}
+  validates :address, presence: true,
+  					 length: {minimum: 10}
+  validates :zipcode, presence: true,
+  					 length: {minimum: 6}
+  validates :pay_rate,presence: true
+  validates :state_id, presence: true
+  validates :first_name,presence: true
+  validates :last_name,presence: true
+
 end
+
