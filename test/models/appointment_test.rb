ac27917238cb8b65ff47583a891782f641f812fb
BelieveC <cayadav2009@gmail.com>
2016-11-14 23:43:54 +0530

    Add images to heroku

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -4,8 +4,44 @@ class AppointmentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should not save appointment without all params " do
-  	appointment = Appointment.new()
-  	assert_not appointment.save,"Saved the appointment without name"
+  setup do
+  	@appointment = Appointment.new
   end
+
+  test "appointment should not save without name" do
+  	@appointment.name = nil
+  	@appointment.vendor_id = 1
+  	@appointment.dock_id = 1
+  	@appointment.timeslot_id = 1
+  	@appointment.datetime = DateTime.now +10.days
+  	assert_not @appointment.save
+  end	
+
+  test "appointment name should be atleast 5 character" do
+  	@appointment.vendor_id = 1
+  	@appointment.dock_id = 1
+  	@appointment.timeslot_id = 1
+  	@appointment.datetime = Date.today+10.days
+  	@appointment.name = "1234"
+  	assert_not @appointment.save
+  end
+
+  test "appointment name should be unique" do
+  	@appointment.vendor_id = 1
+  	@appointment.dock_id = 1
+  	@appointment.timeslot_id = 1
+  	@appointment.datetime = Date.today+10.days
+  	@appointment.name = "Recommend"
+  	@appointment.save
+
+  	@appointment2 = Appointment.new
+  	@appointment2.name = "Recommend"
+  	@appointment.vendor_id = 1
+  	@appointment.dock_id = 1
+  	@appointment.timeslot_id = 1
+  	@appointment.datetime = Date.today+10.days
+  	assert_not @appointment2.save
+  end
+
+
 end
--- test/models/dock_test.rb
+++ test/models/dock_test.rb
@@ -5,8 +5,25 @@ class DockTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "should not save dock without name" do
-  	dock = Dock.new()
-  	assert_not dock.save,"Saved the dock without name"
+  setup do
+  	@dock = Dock.new
+  end
+
+  test "dock should not save without name" do
+  	@dock.name = nil
+  	assert_not @dock.save
+  end	
+
+  test "dock name should be atleast 5 character" do
+  	@dock.name = "1234"
+  	assert_not @dock.save
+  end
+
+  test "dock name should be unique" do
+  	@dock.name = "Mumbai"
+  	@dock.save
+  	@dock2 = Dock.new
+  	@dock2.name = "Mumbai"
+  	assert_not @dock2.save
   end
 end
