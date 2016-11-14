8f596ed6a9b858167d4f729f71061f546129b83d
BelieveC <cayadav2009@gmail.com>
2016-11-14 11:23:30 +0530

    Add test for validation

--- test/models/vendor_test.rb
+++ test/models/vendor_test.rb
@@ -4,4 +4,9 @@ class VendorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save without name" do
+  	vendor = Vendor.new
+  	assert_not vendor.save
+  end
 end
