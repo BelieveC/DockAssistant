1048e5ac085aefea535c9e51a6f2d948f491f7bf
BelieveC <cayadav2009@gmail.com>
2016-11-15 02:44:23 +0530

    add test for state model

--- test/models/state_test.rb
+++ test/models/state_test.rb
@@ -10,12 +10,12 @@ class StateTest < ActiveSupport::TestCase
 
   test "state should not save without name" do
   	@state.name = nil
-  	assert_not @state.save
+  	assert_not @state.save,"State without name is accepted"
   end	
 
   test "state name should be atleast 5 character" do
   	@state.name = "1234"
-  	assert_not @state.save
+  	assert_not @state.save,"State with name less than 5 char is accepted"
   end
 
   test "state name should be unique" do
@@ -23,7 +23,7 @@ class StateTest < ActiveSupport::TestCase
   	@state.save
   	@state2 = State.new
   	@state2.name = "Maharashtra"
-  	assert_not @state2.save
+  	assert_not @state2.save,"State without unique name is accepted"
   end
 
 end
