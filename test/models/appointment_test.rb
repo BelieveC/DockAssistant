3d6bd29a46e5f04dae6452d7f5602d964edbbfff
BelieveC <cayadav2009@gmail.com>
2016-11-14 11:24:05 +0530

    Add test for validation in Appointment

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -4,4 +4,8 @@ class AppointmentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save appointment without all params " do
+  	appointment = Appointment.new()
+  	assert_not appointment.save,"Saved the appointment without name"
+  end
 end
