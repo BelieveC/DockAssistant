c802fd1801f859f5650858eb7a8b8fe00937896d
BelieveC <cayadav2009@gmail.com>
2016-11-15 02:47:06 +0530

    Fix appointments model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,7 +7,9 @@ class Appointment < ApplicationRecord
 
   validates :datetime, presence: true,
   					   inclusion: {in: (Date.today..Date.today+5.years),message: "date should be in future"}
-  # validates :name, presence: true
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: {minimum: 5}
   validates :vendor_id,presence: true
   validates :dock_id, presence: true
   validates :timeslot_id,presence: true
