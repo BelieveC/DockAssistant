7ddde60e7643753d4d4acc428bf3e63736a0da0d
BelieveC <cayadav2009@gmail.com>
2016-11-15 02:44:39 +0530

    add test for vendor model

--- test/models/vendor_test.rb
+++ test/models/vendor_test.rb
@@ -11,12 +11,12 @@ class VendorTest < ActiveSupport::TestCase
 
   test "vendor should not save without name" do
   	@vendor.name = nil
-  	assert_not @vendor.save
+  	assert_not @vendor.save,"Vendor without name is accepted"
   end	
 
   test "vendor name should be atleast 5 character" do
   	@vendor.name = "1234"
-  	assert_not @vendor.save
+  	assert_not @vendor.save,"Vendor with name less than 5 character is accepted"
   end
 
   test "vendor name should be unique" do
@@ -24,6 +24,6 @@ class VendorTest < ActiveSupport::TestCase
   	@vendor.save
   	@vendor2 = Vendor.new
   	@vendor2.name = "Clarkes"
-  	assert_not @vendor2.save
+  	assert_not @vendor2.save,"Vendor without unique name is accepted"
   end
 end
