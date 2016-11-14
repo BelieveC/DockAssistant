0bb256ced748ab10a3a5b46c3d19a414484e12ff
BelieveC <cayadav2009@gmail.com>
2016-11-14 11:23:50 +0530

    Add test for validation in dock_worker

--- test/models/dock_worker_test.rb
+++ test/models/dock_worker_test.rb
@@ -4,4 +4,9 @@ class DockWorkerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save dock_worker without all params " do
+  	dock_worker = DockWorker.new()
+  	assert_not dock_worker.save,"Saved the dock_worker without name"
+  end
 end
